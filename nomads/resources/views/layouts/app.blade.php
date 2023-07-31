580dc1bf149c6dd7b4738a34de2d165549c6484f
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2023-07-31 21:42:07 +0700

    Convertion Landing Page & Detail Page into Laravel
    Public>frontend into View Laravel

--- nomads/resources/views/layouts/app.blade.php
+++ nomads/resources/views/layouts/app.blade.php
@@ -7,136 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>@yield('title')</title>
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css') }}" />
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{ url('frontend/styles/main.css') }}" />
-
-    <!-- Googel Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Assistant:200,400,700&&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet" />
+    @stack('prepend-style')
+    @include('includes.style')
+    @stack('addon-style')
 </head>
 
 <body>
-    <!-- Semantic elements -->
-    <!-- https://www.w3schools.com/html/html5_semantic_elements.asp -->
-
-    <!-- Bootstrap navbar example -->
-    <!-- https://www.w3schools.com/bootstrap4/bootstrap_navbar.asp -->
-
-    <!-- Navbar -->
-    <div class="container">
-        <nav class="row navbar navbar-expand-lg navbar-light bg-white">
-            <a class="navbar-brand" href="index.html">
-                <img src="frontend/images/logo.png" alt="" />
-            </a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navb">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <!-- Menu -->
-            <div class="collapse navbar-collapse" id="navb">
-                <ul class="navbar-nav ml-auto mr-3">
-                    <li class="nav-item mx-md-2">
-                        <a class="nav-link active" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a class="nav-link" href="#">Paket Travel</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                            Services
-                        </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Link 1</a>
-                            <a class="dropdown-item" href="#">Link 2</a>
-                            <a class="dropdown-item" href="#">Link 3</a>
-                        </div>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a class="nav-link" href="#">Testimonial</a>
-                    </li>
-                </ul>
-
-                <!-- Mobile button -->
-                <form class="form-inline d-sm-block d-md-none">
-                    <button class="btn btn-login my-2 my-sm-0">Masuk</button>
-                </form>
-                <!-- Desktop Button -->
-                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                        Masuk
-                    </button>
-                </form>
-            </div>
-        </nav>
-    </div>
-
+    @include('includes.navbar')
     @yield('content')
+    @include('includes.footer')
 
-    <!-- Footer -->
-    <footer class="section-footer mt-5 mb-4 border-top">
-        <div class="container pt-5 pb-5">
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-12 col-lg-3">
-                            <h5>FEATURES</h5>
-                            <ul class="list-unstyled">
-                                <li><a href="#">Reviews</a></li>
-                                <li><a href="#">Community</a></li>
-                                <li><a href="#">Social Media Kit</a></li>
-                                <li><a href="#">Affiliate</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-12 col-lg-3">
-                            <h5>ACCOUNT</h5>
-                            <ul class="list-unstyled">
-                                <li><a href="#">Refund</a></li>
-                                <li><a href="#">Security</a></li>
-                                <li><a href="#">Rewards</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-12 col-lg-3">
-                            <h5>COMPANY</h5>
-                            <ul class="list-unstyled">
-                                <li><a href="#">Career</a></li>
-                                <li><a href="#">Help Center</a></li>
-                                <li><a href="#">Media</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-12 col-lg-3">
-                            <h5>GET CONNECTED</h5>
-                            <ul class="list-unstyled">
-                                <li><a href="#">Jakarta Selatan</a></li>
-                                <li><a href="#">Indonesia</a></li>
-                                <li><a href="#">0821 - 2222 - 8888</a></li>
-                                <li><a href="#">support@nomads.id</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- <div class="copyright border-top text-center">
-        <p>2023 Copyright Nomads • All rights reserved • Made in Jakarta</p>
-      </div> -->
-
-        <div class="container-fluid">
-            <div class="row border-top justify-content-center align-item-center pt-4">
-                <div class="col-auto text-gray-500 font-weight-light">
-                    2023 Copyright Nomads • All rights reserved • Made in Jakarta
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <script src="{{ url('frontend/libraries/retina/retina.js') }}"></script>
-    <script src="{{ url('frontend/libraries/jquery/jquery-3.4.1.min.js') }}"></script>
-    <script src="{{ url('frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
 </body>
 
 </html>
--- nomads/routes/web.php
+++ nomads/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+Route::get('/detail', [DetailController::class, 'index'])
+    ->name('detail');
 
 // Route::get('admin', [DashboardController::class, 'index'])->name('admin');
 Route::prefix('admin')
